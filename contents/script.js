ceced5edbdb71485fa3e44dcd4bc1b83b0c55dc1
FlipArt <gamer76upDATE@outlook.com>
2025-02-16 17:09:54 +0100

    Updated "script.js" to make deleting data work

--- contents/script.js
+++ contents/script.js
@@ -6,6 +6,12 @@ const app = express(); // Create an Express Application
 const port = 3000; // Set the server port
 const db = require("./database.js"); // Import the database file
 
+const getUsernameByID = `SELECT name FROM chat WHERE id = ?`;
+const getUserByID = `SELECT * FROM chat WHERE id = ?`;
+const getAllUsers = `SELECT * FROM chat`;
+const addUser = `INSERT INTO chat (name, age) VALUES (?,?)`;
+const deleteUserByID = `DELETE FROM chat WHERE id = ?`;
+
 app.use(express.json())
 
 console.log("Starting Server..."); // Log message when server is starting
@@ -18,7 +24,7 @@ app.get("/", (req, res) => {
 // Route to get all users
 app.get("/users", (req, res) => {
   console.log("Fetching all users");
-  const users = db.prepare("SELECT * FROM chat").all();
+  const users = db.prepare(getAllUsers).all();
   res.json(users);
 });
 
@@ -30,7 +36,7 @@ app.get("/users/:id", (req, res) => {
   }
 
   console.log(`Fetching user with ID: ${userId}`);
-  const user = db.prepare("SELECT * FROM chat WHERE id = ?").get(userId);
+  const user = db.prepare(getUserByID).get(userId);
 
   if (user) {
     res.json(user);
@@ -43,16 +49,34 @@ app.get("/users/:id", (req, res) => {
 app.post("/users/", (req, res) => {
   const {name, age} = req.body;
   if (!(name && age)) {
-    return res.status(400).json({error: "Provide name and age to add it to the database."})
+    return res.status(400).json({error: "Provide name and age correctly to add it to the database."});
   }
 
   console.log(`Adding Data to Database: Name = ${name} Age = ${age}`)
-  const result = db.prepare("INSERT INTO chat (name, age) VALUES (?,?)").run(name, age)
+  const result = db.prepare(addUser).run(name, age);
 
   if (result.changes > 0) {
-    res.status(201).json({message: "Successfully added user data!"})
+    res.status(201).json({message: "Successfully added user data!"});
   } else {
-    res.status(500).json({error: "An unknown server error ocurred."})
+    res.status(500).json({error: "An unknown server error ocurred."});
+  }
+})
+
+//Route to delete data from the database
+app.delete("/users/", (req, res) => {
+  const {id} = req.body;
+  if (!(id) || isNaN(id)) {
+    return res.status(400).json({error: "Provide the user ID correctly to delete it from the database."});
+  }
+
+  const prevID = db.prepare(getUsernameByID).get(id);
+  console.log(`Deleting Data from Database: Name = ${prevName} ID = ${id}`);
+  const results = db.prepare(deleteUserByID).run(id);
+
+  if (results > 0) {
+    res.status(200).json({message: "Successfully deleted user data!"});
+  } else {
+    res.status(500),json({error: "An unknown server error ocurred."});
   }
 })
 
